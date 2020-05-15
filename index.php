407716c879d2fed050e2f812d2bc675f521a6427
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-05-15 13:13:59 +0100

    - Lines 16 - 21 registered the editor styles for the block - Lines 23 - 28 registered the style for the front-end display of the block - Line 31 tell WP which style file to use for the front end display of the block - Line 32 tell WP which style file to use for the editor

--- index.php
+++ index.php
@@ -13,8 +13,25 @@
          $assets_file['version']
      );
 
+     wp_register_style(
+         'gutenberg_flex_row_editor',
+         plugins_url('css/editor.css', __FILE__),
+         array( 'wp-edit-blocks' ),
+         filemtime( plugin_dir_path( __FILE__ ) )
+     );
+     
+     wp_register_style(
+         'gutenberg_flex_row',
+         plugins_url('css/style.css', __FILE__),
+         array( ),
+         filemtime( plugin_dir_path( __FILE__ ) )
+     );
+     
      register_block_type('gutenberg-examples/gutenberg-flex-row', array(
+         'style'            =>  'gutenberg_flex_row',
+         'editor_style'     =>  'gutenberg_flex_row_editor',
          'editor_script'    =>  'gutenberg_flex_row'
      ));
+     
  };
  add_action('init', 'gutenberg_flex_row_register_block');
