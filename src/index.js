a06c4c57d7d635f2cec45c9b131c155920405987
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-05-15 14:40:10 +0100

    - imported AlignmentToolbar and BlockControls on lines 4,5 - Lines 21 - 25 added props object to handle the alignment attribute of the component - Line 30 added default example behaviour for alignment - Line 36 added alignmnent to functions in edit - lines 44 - 49 Added the alignment change function which will be called when someone clchanged the alignment setting on the alignment toolbar - Lines 59 - 61 Added a call to the BlockControls component which it encapsulating the AlignmentToolbar component which is assigned two props: the current value and the callback to run when it is changed - Line 66 Added style attribute to reflect the editor's current state - line 79 Added the ms-flex-row- class to the rendered component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { registerBlockType } from "@wordpress/blocks";
+import {
+  RichText,
+  AlignmentToolbar,
+  BlockControls,
+} from "@wordpress/block-editor";
 
 registerBlockType("gutenberg-examples/flex-row", {
   title: "Example: Basic",
@@ -8,30 +13,70 @@ registerBlockType("gutenberg-examples/flex-row", {
   // Below tells to get the content of the saved elements paragraph content
   attributes: {
     content: {
-        type: 'array',
-        source: 'children',
-        selector: 'p'
-    }
+      type: "array",
+      source: "children",
+      selector: "p",
+    },
+    // Add props parameter for the alignment set by the user
+    alignment: {
+      type: "string",
+      default: "none",
+    },
   },
   // Give some demo content to the object
   example: {
-      attributes: {
-          content: 'Hello Universe!'
-      }
-  }, 
-  edit: ( props ) => {
+    attributes: {
+      content: "Hello Universe!",
+      alignment: "left",
+    },
+  },
+  edit: (props) => {
     // Get the attributes from the props
-    const { attributes: { content }, setAttributes, className } = props;
+    const {
+      attributes: { content, alignment },
+      setAttributes,
+      className,
+    } = props;
     // Set the onChange event a la react
-    const onChangeContent = newContent => setAttributes({ content: newContent });
+    const onChangeContent = (newContent) =>
+      setAttributes({ content: newContent });
+
+    const onChangeAlignment = (newAlignment) => {
+        console.log(newAlignment);
+      setAttributes({
+        alignment: newAlignment === undefined ? "none" : newAlignment,
+      });
+    };
     // Return the React component to the editor
-    return <RichText 
-        tagName="p"
-        className={ className }
-        onChange={ onChangeContent }
-        value={ content }
-    />;
+    return (
+      <div>
+        {
+          /**
+           * Use the imported blockControls element to
+           * tell the editor to display the alignment toolbar and pass the functionn
+           * to use as a callback in the props.
+           */
+          <BlockControls>
+            <AlignmentToolbar value={alignment} onChange={onChangeAlignment} />
+          </BlockControls>
+        }
+        <RichText
+          tagName="p"
+          // Assign the text alignment using style attribute
+          style={{ textAlign: alignment }}
+          className={className}
+          onChange={onChangeContent}
+          value={content}
+        />
+      </div>
+    );
   },
   // Just return the component on the save function
-  save: props  => <RichText.content tagName="p" value={ props.attributes.content } />,
+  save: (props) => (
+    <RichText.Content
+      tagName="p"
+      value={props.attributes.content}
+      className={`ms-flex-row-${props.attributes.alignment}`}
+    />
+  ),
 });
