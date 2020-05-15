b8bae99c277936f2a93b5e3f5fc3b80947f641cb
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-05-15 13:38:22 +0100

    - Added the attributes object on lines 9 - 15 - Added demo content to the exmaple key on lines 17 - 20 - Lines 22 - 33 changed the edit method to essentially become a react function component which takes the props and then updates on the screena s the user edits the block. Does this using the props object which is passes in the registerblock attributes key. - Lines 35 - 36 Changes the save method to return a RichText.content react component which is the content of the saved component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,34 @@ registerBlockType("gutenberg-examples/flex-row", {
   title: "Example: Basic",
   icon: "universal-access-alt",
   category: "layout",
-  example: {},
-  edit({ className }) {
-    return <p className={className}>Hey there from the green editor</p>;
+  // Tell WP scripts what we would like to get in the attributes object passed as props
+  // Below tells to get the content of the saved elements paragraph content
+  attributes: {
+    content: {
+        type: 'array',
+        source: 'children',
+        selector: 'p'
+    }
   },
-  save() {
-    return <p>Hey there front end is red</p>;
+  // Give some demo content to the object
+  example: {
+      attributes: {
+          content: 'Hello Universe!'
+      }
+  }, 
+  edit: ( props ) => {
+    // Get the attributes from the props
+    const { attributes: { content }, setAttributes, className } = props;
+    // Set the onChange event a la react
+    const onChangeContent = newContent => setAttributes({ content: newContent });
+    // Return the React component to the editor
+    return <RichText 
+        tagName="p"
+        className={ className }
+        onChange={ onChangeContent }
+        value={ content }
+    />;
   },
+  // Just return the component on the save function
+  save: props  => <RichText.content tagName="p" value={ props.attributes.content } />,
 });
