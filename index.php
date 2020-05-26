a0ee9abcd13dead6e30d92e3f1d828457c34b48b
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-05-26 12:07:22 +0100

    - Lines 2 - 13 added wordpress plugin comment with correct information about the plugin - Line 16 changed the name of the register function - Line 21 change the name of the js script - Line 23 added wp-editor so the script is loaded in the editor - Lines 29 - 32 change the name of the style being imported - Lines 36 - 39 changed the name of the editor style - Lines 42 - 47 amended names in the register function - Line 49 amended the name in the add_action

--- index.php
+++ index.php
@@ -1,37 +1,49 @@
-<?php 
+<?php
+
 /**
- * Plugin Name: Gutenberg Flex Row Block
+ * Plugin Name: Making Stuffs Blocks
+ * Plugin URI: https://makingstuffs.co.uk/making-stuffs-blocks
+ * Description: A collection of simple reusable Gutenberg blocks which can be used throughout 
+ * your website to alter its display.
+ * Version: 0.0.6
+ * Author: Making Stuffs
+ * Author URI: https://makingstuffs.co.uk
+ * Licence: MIT
+ * Text Domain: msblocks
+ * 
  */
 
- function gutenberg_flex_row_register_block() {
-     $assets_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' );
+function msblocks_register () {
 
-     wp_register_script(
-         'gutenberg_flex_row',
-         plugins_url('/build/index.js', __FILE__),
-         $assets_file['dependencies'],
-         $assets_file['version']
-     );
+    $assets_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' );
+    
+    wp_register_script(
+        'msblocks-script', 
+        plugins_url('/build/index.js', __FILE__),
+        array('wp-editor'),
+        $assets_file['dependencies'],
+        $assets_file['version']
+    );
 
-     wp_register_style(
-         'gutenberg_flex_row_editor',
-         plugins_url('css/editor.css', __FILE__),
-         array( 'wp-edit-blocks' ),
-         filemtime( plugin_dir_path( __FILE__ ) )
-     );
-     
-     wp_register_style(
-         'gutenberg_flex_row',
-         plugins_url('css/style.css', __FILE__),
-         array( ),
-         filemtime( plugin_dir_path( __FILE__ ) )
-     );
-     
-     register_block_type('gutenberg-examples/gutenberg-flex-row', array(
-         'style'            =>  'gutenberg_flex_row',
-         'editor_style'     =>  'gutenberg_flex_row_editor',
-         'editor_script'    =>  'gutenberg_flex_row'
-     ));
-     
- };
- add_action('init', 'gutenberg_flex_row_register_block');
+    wp_register_style(
+        'msblocks-style',
+        plugins_url('/build/css/style.css', __FILE__),
+        array(),
+        filemtime(plugin_dir_path(__FILE__) . 'build/css/style.css')
+    );
+
+    wp_register_style(
+        'msblocks-style-editor',
+        plugins_url('/build/css/editor.css', __FILE__),
+        array( 'wp-edit-blocks' ),
+        filemtime(plugin_dir_path(__FILE__) . 'build/css/editor.css')
+    );
+
+    register_block_type('msblocks/ms-blocks', array(
+        'style'         =>  'msblocks-style',
+        'editor_style'  =>  'msblocks-style-editor',
+        'editor_script' =>  'msblocks-script'
+    ));
+};
+
+add_action('init', 'msblocks_register');
