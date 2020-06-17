be8c490db2bf1a758565e0ed589d6880419a165e
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-06-17 18:17:12 +0100

    Added the filterableProducstByCategory block to the queue

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import { __ } from "@wordpress/i18n";
 import { registerBlockType } from "@wordpress/blocks";
 import * as imageTextFlexRow from "./blocks/image-text-flex-row";
 import * as htmlTextFlexRow from "./blocks/text-html-flex-row";
-import './style.scss';
-import './editor.scss';
+import * as filterableProductsByCategory from "./blocks/filterable-products-by-category";
+import "./sass/style.scss";
+import "./sass/editor.scss";
 
 (() => {
-  const blocks = [imageTextFlexRow, htmlTextFlexRow];
+  const blocks = [
+    imageTextFlexRow,
+    htmlTextFlexRow,
+    filterableProductsByCategory,
+  ];
 
   blocks.forEach((block) => {
-      registerBlockType(block.name, block.settings);
-    });
+    registerBlockType(block.name, block.settings);
+  });
 })();
