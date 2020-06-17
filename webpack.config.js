4c73813b39da37d39627ee2e86d5ef8ae83295f4
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-06-17 18:07:42 +0100

    - Added new entrys - Line 30: Corrected importLoaders - Line 37: Removed browserlists to the package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,49 @@ const defaultConfig = require("./node_modules/@wordpress/scripts/config/webpack.
 const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
-    ...defaultConfig,
-    module: {
-      ...defaultConfig.module,
-      rules: [
-        ...defaultConfig.module.rules,
-        {
-          test: /\.(sc|sa|c)ss$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "css/[name].css"
-              }
+  ...defaultConfig,
+  entry: {
+    index: './src/index.js',
+    bundle: './src/js/bundle.js'
+  },
+  module: {
+    ...defaultConfig.module,
+    rules: [
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "css/[name].css",
             },
-            {
-              loader: "extract-loader"
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              importLoaders: 3,
             },
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true,
-                importLoaders: 2
-              }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [postcssPresetEnv()],
+              sourceMap: "inline",
             },
-            {
-              loader: "postcss-loader",
-              options: {
-                ident: "postcss",
-                plugins: () => [postcssPresetEnv({browserslist: [
-                  "defaults",
-                  "last 2 versions",
-                  "> 0.2%",
-                  "not dead"
-                ]})],
-                sourceMap: 'inline'
-              }
-            },
-                          {
-                            loader: "sass-loader",
-                            options: {sourceMap: true}
-                          },
-          ]
-        }
-      ]
-    },
-    plugins: [...defaultConfig.plugins]
-  };
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      ...defaultConfig.module.rules,
+    ],
+  },
+  plugins: [...defaultConfig.plugins],
+};
